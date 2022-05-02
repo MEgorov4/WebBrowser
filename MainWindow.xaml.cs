9e9d498a3cc6c4025a3a53ccba93e612155041a9
mishail6667 <mishail6667@gmail.com>
2022-05-02 19:55:41 +0500

    Изменения формы

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,41 @@ namespace WebBrowser
     /// </summary>
     public partial class MainWindow : Window
     {
+        private void UpdateButton(object sender, RoutedEventArgs e)
+        {
+            /*Pass*/
+        }
+        private void Browser_AddressChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            /*Pass*/
+        }
+        private void AddressButton(object sender, RoutedEventArgs e)
+        {
+            ChangeAdress();
+        }
+        private void ChangeAdress()
+        {
+            Browser.Address = AddressBar.Text;
+        }
+        private void UpdateAddressBar()
+        {
+            AddressBar.Text = Browser.Address;
+        }
+
+        private void AddressBar_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                ChangeAdress();
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            Browser.AddressChanged += Browser_AddressChanged;
+            UpdateAddressBar();
         }
+
     }
 }
